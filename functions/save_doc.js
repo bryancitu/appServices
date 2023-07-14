7b1745447b24e8b26444ccb8d0b9f0dccb44d8d9
Bryan Jesus Ramon Avila <bryanjesus.ra@gmail.com>
2023-07-14 13:27:05 -0500

    feat: connecting mongodb

--- functions/save_doc.js
+++ functions/save_doc.js
@@ -7,11 +7,12 @@ exports = async function(arg){
   var serviceName = "mongodb-atlas";
 
   // Update these to reflect your db/collection
-  var dbName = "db_name";
-  var collName = "coll_name";
+  var dbName = "file";
+  var collName = "docs";
 
   // Get a collection from the context
   var collection = context.services.get(serviceName).db(dbName).collection(collName);
+  console.log("collection", collection);
 
   var findResult;
   try {
